4d6b86753256d035ddcef00eaab7217a6fb102c3
Sei <reneebernardflores@gmail.com>
2025-09-25 16:24:13 +0800

    Update plantsModel.js

--- backend/models/plantsModel.js
+++ backend/models/plantsModel.js
@@ -3,9 +3,25 @@ const Schema = mongoose.Schema;
 
 //creates a schema
 const plantSchema = new Schema({
-    name: {type: String, required: true},
-    species: {type: String, required: true},
-    description: {type: String, required: true}
+    common_name: {
+        type: [String], 
+        required: true,
+        validate:{
+            validator: function(v) {
+                return v.length > 0; // Ensures the string is not empty
+            }
+        },
+        message: props => `${props.value} must contain at least one common name!`
+
+    },
+    Family: {
+        type: String, 
+        required: true
+    },
+    description: {
+        type: String, 
+        required: true
+    }
 }, {timestamps: true});
 
 //creates a model and exports it to the routes and controllers
