dd567b2c83eaa936a9230c7a6ad70b8898f27656
Sei <reneebernardflores@gmail.com>
2026-01-31 00:05:12 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ const gardenRoutes = require('./routes/garden');
 
     //middleware
     app.use(cors());
-    app.use(express.json());
+    app.use(express.json({ limit: '10mb' }));
+    app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
     // Set up rate limiting
     const weatherApiLimiter = rateLimit({
