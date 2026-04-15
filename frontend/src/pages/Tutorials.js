0602f7d4e20fe38dac9d6bb9f99298347bb6653d
Sei <reneebernardflores@gmail.com>
2026-04-16 00:15:19 +0800

    Update Tutorials.js

--- frontend/src/pages/Tutorials.js
+++ frontend/src/pages/Tutorials.js
@@ -99,7 +99,7 @@ export default function Tutorials() {
                 alt={light.name} 
                 style={{ 
                   width: "100%", 
-                  height: "200px", 
+                  height: "300px", 
                   objectFit: "cover", 
                   borderRadius: "8px", 
                   marginBottom: "16px",
