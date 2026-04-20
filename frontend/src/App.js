e331ec9e746cccc1afc62e8c09345ae11eb97a2b
Sei <reneebernardflores@gmail.com>
2026-04-21 00:40:25 +0800

    Fixed a typo that crashed the whole site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -152,9 +152,9 @@ export default function App() {
           <Route
             path="/admin-reports" 
             element={
-              
+              <RequireAuth>
                 <AdminReports />
-              
+              </RequireAuth>
             }
           />
 
